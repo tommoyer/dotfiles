d83c01cc9de48cbafa25a0eba1963ebb89be69d0
Tom Moyer <tom.moyer@uncc.edu>
2023-06-22 16:35:50 -0400

    Added more to newproj and included a basic kernel build deps script

---
+++ apps/Applications/kern-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install -y dh-systemd makedumpfile libnewt-dev libiberty-dev default-jdk-headless java-common libdw-dev libpci-dev libunwind8-dev liblzma-dev libaudit-dev uuid-dev libnuma-dev lz4 dwarves libcap-dev pkg-config zstd git gnupg pbuilder ubuntu-dev-tools apt-file build-essential gcc libssl-dev grub2 bc autoconf automake autotools-dev debmake dh-make kernel-wedge devscripts debhelper ccache libtool libncurses5 libncurses5-dev libelf-dev libosinfo-bin flex bison dkms
