6c09268542e716f75ae2fce4b25c5c5a62304e7d
Tom Moyer <tom.moyer@uncc.edu>
2023-06-26 12:08:40 -0400

    Cleanup some old scripts

--- apps/Applications/bootstrap-lxc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lxc launch ubuntu:14.04 trusty-14-04
-lxc launch ubuntu:16.04 xenial-16-04
-lxc launch ubuntu:18.04 bionic-18-04
-lxc launch ubuntu:20.04 focal-20-04
-lxc launch ubuntu:22.04 jammy-22-04
