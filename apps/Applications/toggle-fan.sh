2e2f61996a9300cf9dc722196deaea1149b1abbc
Tom Moyer <tom.moyer@uncc.edu>
2023-07-11 15:45:57 -0400

    Added scripts for fan and light

---
+++ apps/Applications/toggle-fan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kasa_cmd=/home/tmoyer/.local/bin/kasa
+fan_hostname=desk-fan.home
+
+fan_state=$(${kasa_cmd} --host ${fan_hostname} --type plug --json state | jq '.system.get_sysinfo.relay_state')
+
+if [[ "${fan_state}" == "1" ]]
+then
+    # echo "Fan is on"
+    ${kasa_cmd} --host ${fan_hostname} --type plug off &> /dev/null
+else
+    # echo "Fan is off"
+    ${kasa_cmd} --host ${fan_hostname} --type plug on &> /dev/null
+fi
---
+++ apps/Applications/toggle-light.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kasa_cmd=/home/tmoyer/.local/bin/kasa
+light_hostname=desk-light.home
+
+light_state=$(${kasa_cmd} --host ${light_hostname} --type plug --json state | jq '.system.get_sysinfo.relay_state')
+
+if [[ "${light_state}" == "1" ]]
+then
+    # echo "light is on"
+    ${kasa_cmd} --host ${light_hostname} --type plug off &> /dev/null
+else
+    # echo "light is off"
+    ${kasa_cmd} --host ${light_hostname} --type plug on &> /dev/null
+fi
