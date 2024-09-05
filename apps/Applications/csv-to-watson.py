0a0a8c45d3e96f59fc057f4a64c59c86f2441e7a
Tom Moyer <tommoyer@gmail.com>
2024-09-05 16:52:44 -0400

    more

--- apps/Applications/csv-to-watson.py
+++ apps/Applications/csv-to-watson.py
@@ -5,4 +5,4 @@ import csv
 with open('timecards.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(f'watson add -c -b --from "{row["date"]} {row["start time"]}" --to "{row["date"]} {row["end time"]}" {row["project"]} --note "{row["note"]}"')
+        print(f'watson add --from "{row["date"]} {row["start time"]}" --to "{row["date"]} {row["end time"]}" {row["project"]} --note "{row["note"]}"')
