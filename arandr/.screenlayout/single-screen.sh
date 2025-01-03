4b1cc07ffa62081d11680047a41663f0186f5298
Tom Moyer <tommoyer@gmail.com>
2025-01-02 20:32:20 -0500

    more

---
+++ arandr/.screenlayout/single-screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 3840x2400 --pos 0x0 --rotate normal --scale 0.5 --filter nearest --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off
