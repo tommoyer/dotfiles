54cd01a35e927af710f75737d09ac8331142c86d
Tom Moyer <tommoyer@gmail.com>
2025-01-02 21:23:01 -0500

    more

---
+++ screens/.screenlayout/portable-monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 3840x2400 --pos 0x0 --scale 0.5 --filter nearest --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --mode 3840x2400 --pos 3840x0 --rotate normal --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off
